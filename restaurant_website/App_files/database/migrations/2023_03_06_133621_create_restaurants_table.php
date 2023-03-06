5f4cb43ad15e2e43cc1b01be53adc5d8d27eb355
SeanKellyIADT <xseankellyx@gmail.com>
2023-03-06 13:38:50 +0000

    Created migration for main table (restaurants)

---
+++ restaurant_website/App_files/database/migrations/2023_03_06_133621_create_restaurants_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('Name');
+            $table->string('Cuisine');
+            $table->string('County');
+            $table->integer('Rating');
+            $table->string('Bio');
+            $table->integer('Price-range');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restuarants');
+    }
+};
