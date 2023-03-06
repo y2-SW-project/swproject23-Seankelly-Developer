204c1aae20ba1d60ed571f8e6b2f73934b9411d5
SeanKellyIADT <xseankellyx@gmail.com>
2023-03-06 16:25:16 +0000

    homepage work

--- restaurant_website/App_files/resources/views/welcome.blade.php
+++ restaurant_website/App_files/resources/views/welcome.blade.php
@@ -5,15 +5,15 @@
         <div class="row justify-content-center">
             <div>
 
-                <div class="row row-cols-1 row-cols-md-12 g-4 mt-5 col-m">
+                <div class="row row-cols-1 row-cols-md-12 g-4 mt-5 col-m text-center align-items-center">
                     <div class="col-6 ">
-                        <div class="card">
+                        {{-- <div class="card"> --}}
                         
                             <div class="card-body">
-                                <h5 class="card-title">Make a Booking</h5>
-                                
+                                <h5 class="card-title">Insert Slogan</h5>
+                                <p>Insert business decription </p>
                             </div>
-                        </div>
+                        {{-- </div> --}}
                     </div>
                     <div class="col-6">
                         <div class="card">
