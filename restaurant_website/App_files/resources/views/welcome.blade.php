3769869fb2b142cb2b2c9b084a58745632dde4de
SeanKellyIADT <xseankellyx@gmail.com>
2023-03-06 16:10:22 +0000

    home page started

--- restaurant_website/App_files/resources/views/welcome.blade.php
+++ restaurant_website/App_files/resources/views/welcome.blade.php
@@ -5,29 +5,27 @@
         <div class="row justify-content-center">
             <div>
 
-                <div class="row row-cols-1 row-cols-md-12 g-4 mt-5">
-                    <div class="col-6">
+                <div class="row row-cols-1 row-cols-md-12 g-4 mt-5 col-m">
+                    <div class="col-6 ">
                         <div class="card">
-                            <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-social.png"
-                                class="card-img-top" alt="...">
+                        
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                                    to additional content. This content is a little bit longer.</p>
+                                <h5 class="card-title">Make a Booking</h5>
+                                
                             </div>
                         </div>
                     </div>
                     <div class="col-6">
                         <div class="card">
-                            <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-social.png"
-                                class="card-img-top" alt="...">
+                            <img src={{ url('storage/viewAll.jpg') }}
+                                class="card-img-top"  alt="...">
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                                    to additional content. This content is a little bit longer.</p>
+                                <h5 class="card-title">View All Restaurants!</h5>
+                                
                             </div>
                         </div>
                     </div>
+                    
                     
                     
                     </div>
