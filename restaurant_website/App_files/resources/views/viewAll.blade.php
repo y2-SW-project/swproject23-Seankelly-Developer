053481dc64aa28f1c4b940b6f34787bbbc180af7
SeanKellyIADT <xseankellyx@gmail.com>
2023-03-14 21:02:25 +0000

    Added button on viewAll view for menu's

--- restaurant_website/App_files/resources/views/viewAll.blade.php
+++ restaurant_website/App_files/resources/views/viewAll.blade.php
@@ -1,35 +1,37 @@
-@extends('layouts.app') <!-- This line extends the main layout from the app.blade.php file -->
+@extends('layouts.app')
 
-@section('content') <!-- This section defines the content of the page -->
-
-<header class="bg-light"> <!-- This is the header section of the page with a light background color -->
+@section('content')
+<header class="bg-light">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h1 class="text-center display-4 py-5"> There's something for everyone!</h1> <!-- This is the title of the page -->
+                <h1 class="text-center display-4 py-5"> There's something for everyone!</h1>
             </div>
         </div>
     </div>
 </header>
-<div class="container my-5"> <!-- This container holds the content of the page -->
+<div class="container my-5">
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="row row-cols-1 row-cols-md-2 g-4">
-                <!-- This loop iterates through the $restaurants array and displays the data of each restaurant in a card -->
                 @foreach ($restaurants as $restaurant)
                     <div class="col-sm-6">
                         <div class="card">
-                            <img src="{{ url('storage/viewAll.jpg') }}" class="card-img-top" alt="..."> <!-- This is the image of the restaurant -->
+                            <img src="{{ url('storage/viewAll.jpg') }}" class="card-img-top" alt="...">
                             <div class="card-body">
-                                <h2 class="card-title">{{ $restaurant->Name }}</h2> <!-- This is the name of the restaurant -->
+                                <h2 class="card-title">{{ $restaurant->Name }}</h2>
                                 <p class="card-text">{{ $restaurant->Cuisine }} | {{ $restaurant->County }} | 
                                     @for ($i = 0; $i < $restaurant->Price_range; $i++)
                                         €
                                     @endfor
-                                </p> <!-- This is the cuisine, county and price range of the restaurant -->
-                                <p class="card-text">{{ $restaurant->Bio }}</p> <!-- This is the bio/description of the restaurant -->
+                                </p>
+                                <p class="card-text">{{ $restaurant->Bio }}</p>
                                 <a href="{{ route('reservations.create', $restaurant->id) }}" class="btn btn-primary">Book Now</a>
-                            <!-- This button is used to book the restaurant -->
+                                <a href="#" class="btn btn-secondary">View Menu</a> <!-- Add a new button to view the restaurant's menu -->
+
+
+                                {{-- {{ route('menus.show', $restaurant->id) }} --}}
+
                             </div>
                         </div>
                     </div>
@@ -38,13 +40,13 @@
         </div>
     </div>
 </div>
-<footer class="bg-dark text-light py-3"> <!-- This is the footer section of the page with a dark background color -->
+<footer class="bg-dark text-light py-3">
     <div class="container">
         <div class="row">
             <div class="col-md-12 text-center">
-                <p>&copy; {{ date('Y') }} Restaurant Listings</p> <!-- This displays the current year and the name of the website -->
+                <p>&copy; {{ date('Y') }} Restaurant Listings</p>
             </div>
         </div>
     </div>
 </footer>
-@endsection <!-- This closes the content section of the page -->
+@endsection
