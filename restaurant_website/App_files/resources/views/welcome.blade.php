f9fa607aaae267b5f8c0fb0f7ca4745b76b1e190
SeanKellyIADT <xseankellyx@gmail.com>
2023-04-19 19:16:31 +0100

    success

--- restaurant_website/App_files/resources/views/welcome.blade.php
+++ restaurant_website/App_files/resources/views/welcome.blade.php
@@ -1,75 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-<div class="row justify-content-center">
-<div>
-<div class="row row-cols-1 row-cols-md-12 g-4 mt-5 col-m text-center align-items-center">
-<div class="col-sm-6">
-{{-- <div class="card"> --}}
-    @if (session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card shadow-lg p-3 mb-5 bg-white rounded">
+                <div class="card-body">
+                    @if (session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                    @endif
+                    <h2 class="card-title fw-bold text-center mb-4">Book a Table, Savor the Moment</h2>
+                    <p class="card-text text-center">Our restaurant booking web application is a game-changer for foodies and restaurateurs alike. With our platform, customers can discover new restaurants and book reservations in real-time, while restaurant owners can manage their bookings and customer relationships more efficiently. Our system also integrates with popular POS systems and provides detailed analytics to help restaurants optimize their operations. With our user-friendly interface and cutting-edge technology, we're revolutionizing the restaurant industry. Try us today and take your dining experience to the next level!</p>
+                    <div class="text-center mt-5">
+                        <a href="{{ route('Restaurants.index') }}" class="btn btn-primary">View All Restaurants</a>
+                        @auth
+                            <a href="{{ route('userReservations') }}" class="btn btn-secondary ms-3">My Reservations</a>
+                        @endauth
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-@endif
-<div class="card-body">
-<h5 class="card-title fw-bold">Book a table, savor the moment</h5>
-<p><br>Our restaurant booking web application is a game-changer for foodies and restaurateurs alike. With our platform, customers can discover new restaurants and book reservations in real-time, while restaurant owners can manage their bookings and customer relationships more efficiently. Our system also integrates with popular POS systems and provides detailed analytics to help restaurants optimize their operations. With our user-friendly interface and cutting-edge technology, we're revolutionizing the restaurant industry. Try us today and take your dining experience to the next level!</p>
-</div>
-{{-- </div> --}}
-</div>
-            <div class="col-sm-6">
-            <div class="card">
-            <a href="{{ route('Restaurants.index') }}">
-            <img src="{{ url('storage/viewAll.jpg') }}" class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">View All Restaurants!</h5>
-            @if(auth()->check())
-    <a href="{{ route('userReservations') }}" class="btn btn-primary">My Reservations</a>
-@endif
-</div>
-</a>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
 
-<div class="row justify-content-center mt-5">
-    <div class="col-12 text-center mb-4">
-        <h2 class="fw-bold">Favorites</h2>
-    </div>
-    <div class="col-md-4">
-        <a href="#">
-            <div class="card p-3">
-                <img src="{{ url('/images/image ('. rand(1, 86) .').jpg') }}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title fw-bold mb-0">Italians</h5>
+    <div class="row justify-content-center mt-5">
+        <div class="col-12 text-center mb-4">
+            <h2 class="fw-bold">Favorites</h2>
+        </div>
+        <div class="col-md-4">
+            <a href="#">
+                <div class="card p-3">
+                    <img src="{{ url('/images/image ('. rand(1, 86) .').jpg') }}" class="card-img-top restaurant-image" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title fw-bold mb-0">Italians</h5>
+                    </div>
                 </div>
-            </div>
-        </a>
-    </div>
-    <div class="col-md-4">
-        <a href="#">
-            <div class="card p-3">
-                <img src="{{ url('/images/image ('. rand(1, 86) .').jpg') }}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title fw-bold mb-0">Mexican</h5>
+            </a>
+        </div>
+        <div class="col-md-4">
+            <a href="#">
+                <div class="card p-3">
+                    <img src="{{ url('/images/image ('. rand(1, 86) .').jpg') }}" class="card-img-top restaurant-image" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title fw-bold mb-0">Mexican</h5>
+                    </div>
                 </div>
-            </div>
-        </a>
-    </div>
-    <div class="col-md-4">
-        <a href="#">
-            <div class="card p-3">
-                <img src="{{ url('/images/image ('. rand(1, 86) .').jpg') }}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title fw-bold mb-0">Indian</h5>
+            </a>
+        </div>
+        <div class="col-md-4">
+            <a href="#">
+                <div class="card p-3">
+                    <img src="{{ url('/images/image ('. rand(1, 86) .').jpg') }}" class="card-img-top restaurant-image" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title fw-bold mb-0">Indian</h5>
+                    </div>
                 </div>
-            </div>
-        </a>
+            </a>
+        </div>
     </div>
 </div>
-
 @endsection
