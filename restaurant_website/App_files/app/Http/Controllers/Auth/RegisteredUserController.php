7df37e14b5950e37b577e0787753704fc19f9e58
SeanKellyIADT <xseankellyx@gmail.com>
2023-04-20 13:59:13 +0100

    Added user role to user table

--- restaurant_website/App_files/app/Http/Controllers/Auth/RegisteredUserController.php
+++ restaurant_website/App_files/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use Illuminate\Support\Str;
 
 class RegisteredUserController extends Controller
 {
@@ -32,7 +33,7 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
@@ -41,6 +42,10 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+        if (Str::contains($user->email, '@example.com')) {
+            $user->role = 'restaurant owner';
+            $user->save();
+        }
 
         event(new Registered($user));
 
